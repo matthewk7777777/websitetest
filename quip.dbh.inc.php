6612f030af3e7496a33e152956ca621fcc9cb382
matthewk7777777 <78668737+matthewk7777777@users.noreply.github.com>
2021-04-13 18:18:42 -0700

    Create quip.dbh.inc.php

---
+++ quip.dbh.inc.php
@@ -0,0 +1,21 @@
+<?php
+$conn = mysqli_connect("sql3.freemysqlhosting.net", "sql3404627", "uQiF1rwxn7", "sql3404627", "3306");
+/*if(!$conn){
+    echo "not connected ...";
+}else{
+    echo "Connection successful...";
+} 
+
+  $db_host = 'localhost';
+  $db_user = 'id16574885_stripedb';
+  $db_password = 'z2<b_BPe%uDRnE*i';
+  $db_db = 'id16574885_stripe';
+
+  $conn = mysqli_connect(
+    $db_host,
+    $db_user,
+    $db_password,
+  );
+  */
+  
+  //1}sH/f#rqTU?O7Dq
